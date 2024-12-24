854bdb4ba919ffbecd54226ad74bef5258a4728c
sanbei101 <sanbei101@outlook.com>
2024-12-24 16:42:37 +0800

    数学公式的渲染

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,30 +1,32 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
 export default defineConfig({
   title: "数学重构计划",
   description: "圆曲不联立,导数不求导,高考不报名...",
+  lang: "zh-CN",
+  markdown: {
+    math: true,
+  },
   themeConfig: {
     nav: [
-      { text: '首页', link: '/' },
-      { text: '关于', link: '/about' }
+      { text: "首页", link: "/" },
+      { text: "关于", link: "/about" },
     ],
 
     sidebar: [
       {
-        text: '不等式',
+        text: "不等式",
         collapsed: true,
         items: [
           {
-            text: '极化恒等式',
-            link: '/不等式/极化恒等式'
-          }
-        ]
-
-      }
+            text: "极化恒等式",
+            link: "/不等式/极化恒等式",
+          },
+        ],
+      },
     ],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/sanbei101' }
-    ]
-  }
-})
+    socialLinks: [{ icon: "github", link: "https://github.com/sanbei101" }],
+  },
+  cleanUrls: true,
+});
